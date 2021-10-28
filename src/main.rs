f470c97aa25fb82061ed3740674341c1246a185d
arashigaoka <17226903+arashigaoka@users.noreply.github.com>
2021-10-28 21:39:27 +0900

    enable to scroll

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,7 @@ struct CursorController {
     cursor_y: usize,
     screen_columns: usize,
     screen_rows: usize,
+    row_offset: usize,
 }
 
 impl CursorController {
@@ -80,9 +81,10 @@ impl CursorController {
             cursor_y: 0,
             screen_columns: win_size.0,
             screen_rows: win_size.1,
+            row_offset: 0,
         }
     }
-    fn move_cursor(&mut self, direction: KeyCode) {
+    fn move_cursor(&mut self, direction: KeyCode, number_of_rows: usize) {
         match direction {
             KeyCode::Up => {
                 self.cursor_y = self.cursor_y.saturating_sub(1);
@@ -93,7 +95,7 @@ impl CursorController {
                 }
             }
             KeyCode::Down => {
-                if self.cursor_y != self.screen_rows - 1 {
+                if self.cursor_y < number_of_rows {
                     self.cursor_y += 1;
                 }
             }
@@ -105,6 +107,12 @@ impl CursorController {
             KeyCode::End => self.cursor_x = self.screen_columns - 1,
             KeyCode::Home => self.cursor_x = 0,
             _ => unimplemented!(),
+        }
+    }
+    fn scroll(&mut self) {
+        self.row_offset = cmp::min(self.row_offset, self.cursor_y);
+        if self.cursor_y >= self.row_offset + self.screen_rows {
+            self.row_offset = self.cursor_y - self.screen_rows + 1;
         }
     }
 }
@@ -166,7 +174,8 @@ impl Output {
         let screen_rows = self.win_size.1;
         let screen_columns = self.win_size.0;
         for i in 0..screen_rows {
-            if i >= self.editor_rows.number_of_rows() {
+            let file_row = i + self.cursor_controller.row_offset;
+            if file_row >= self.editor_rows.number_of_rows() {
                 if self.editor_rows.number_of_rows() == 0 && i == screen_rows / 3 {
                     let mut welcome = format!("Pound Editor --- Version {}", "0.1.0");
                     if welcome.len() > screen_columns {
@@ -183,9 +192,9 @@ impl Output {
                     self.editor_contents.push('~');
                 }
             } else {
-                let len = cmp::min(self.editor_rows.get_row(i).len(), screen_columns);
+                let len = cmp::min(self.editor_rows.get_row(file_row).len(), screen_columns);
                 self.editor_contents
-                    .push_str(&self.editor_rows.get_row(i)[..len])
+                    .push_str(&self.editor_rows.get_row(file_row)[..len])
             }
             queue!(
                 self.editor_contents,
@@ -198,10 +207,11 @@ impl Output {
         }
     }
     fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        self.cursor_controller.scroll();
         queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
         self.draw_rows();
         let cursor_x = self.cursor_controller.cursor_x;
-        let cursor_y = self.cursor_controller.cursor_y;
+        let cursor_y = self.cursor_controller.cursor_y - self.cursor_controller.row_offset;
         queue!(
             self.editor_contents,
             cursor::MoveTo(cursor_x as u16, cursor_y as u16),
@@ -210,7 +220,7 @@ impl Output {
         self.editor_contents.flush()
     }
     fn move_cursor(&mut self, direction: KeyCode) {
-        self.cursor_controller.move_cursor(direction);
+        self.cursor_controller.move_cursor(direction, self.editor_rows.number_of_rows());
     }
 }
 
